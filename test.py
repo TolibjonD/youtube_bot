cc91e7775733dc0f8927718ef9b2963e2c26aef5
Saidkodirov <stolibjon123@gmail.com>
2024-07-13 20:10:47 +0500

    sa

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from googletrans import Translator
 
 async def translate(text, to_lang="en", from_lang="uz"):
-    translator = Translator()
-    translated_text = translator.translate(text=text, dest=to_lang)
-    return translated_text.text
+    try:
+        translator = Translator()
+        translated_text = translator.translate(text=text, dest=to_lang)
+        return translated_text.text
+    except:
+        return text
